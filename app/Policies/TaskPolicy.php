2bf67dcd91634c2a6973f3e0a2f348a1f06b69cb
suchanon-todo <suchanon66@gmail.com>
2026-06-18 14:32:22 +0700

    clean: remove dead code

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -4,20 +4,11 @@ namespace App\Policies;
 
 use App\Models\Task;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class TaskPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view the task.
      */
     public function view(User $user, Task $task): bool
     {
@@ -25,15 +16,7 @@ class TaskPolicy
     }
 
     /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can update the model.
+     * Determine whether the user can update the task.
      */
     public function update(User $user, Task $task): bool
     {
@@ -41,26 +24,10 @@ class TaskPolicy
     }
 
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the user can delete the task.
      */
     public function delete(User $user, Task $task): bool
     {
         return $user->id === $task->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Task $task): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Task $task): bool
-    {
-        return false;
     }
 }
