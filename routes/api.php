8b2cc5e5e8cc18c50a4301bc6f438eaa25248e30
suchanon-todo <suchanon66@gmail.com>
2026-06-18 13:10:52 +0700

    feat: Protect Routes — the Middleware (Authentication)

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
 
-Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('tasks', TaskController::class);
+});
+
+
 
 
 
